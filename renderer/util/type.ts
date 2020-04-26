91b522d92a906208bf6b53a008a7407dab0632f0
Ziphil <yfe23457@nifty.com>
2020-04-27 03:31:53 +0900

    変更

--- renderer/util/type.ts
+++ renderer/util/type.ts
@@ -10,6 +10,6 @@ export type Length<A> = A extends {length: infer N} ? N : never;
 
 // 数値リテラル型 N に対し、0 以上 N 以下の整数から成る共用体型を返します。
 // 例えば、OrBelow<3> は 0 | 1 | 2 | 3 に評価されます。
-export type OrBelow<N extends number> = N extends 0 ? never : Length<OrBelowTuple<N>>;
-type OrBelowTuple<N> = OrBelowTupleRec<N, [], []>;
+export type OrBelow<N extends number> = Length<OrBelowTuple<N>>;
+type OrBelowTuple<N extends number> = OrBelowTupleRec<N, [], []>;
 type OrBelowTupleRec<N, R, S extends Array<any>> = {0: R, 1: OrBelowTupleRec<N, R | Append<any, S>, Append<any, S>>}[S extends {length: N} ? 0 : 1];
