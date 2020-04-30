f4c0a23e114841a0fa1ad75c314baa613c4ed06f
Ziphil <yfe23457@nifty.com>
2020-05-01 04:59:01 +0900

    デフォルトのウィンドウサイズを変更

--- main/index.ts
+++ main/index.ts
@@ -101,7 +101,7 @@ class Main {
   }
 
   private createMainWindow(): BrowserWindow {
-    let options = {width: 420, height: 600, minWidth: 420, minHeight: 100};
+    let options = {width: 420, height: 476, minWidth: 420, minHeight: 476};
     let window = this.createWindow("dashboard", null, {}, options);
     this.connectReloadClient(window);
     return window;
